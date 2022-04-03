b108a95b0369fe346e6f9357cf9bf7230ae44bca
Nitin Jiwnani <jiwnani01@gmail.com>
2022-04-04 03:50:51 +0530

    Theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'pages/home_page.dart';
@@ -10,20 +11,16 @@ void main(List<String> args) {
 }
 
 class MyApp extends StatelessWidget {
+  get darkTheme => null;
+
   @override
   Widget build(BuildContext context) {
     
     return MaterialApp(     
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        primaryTextTheme: GoogleFonts.latoTextTheme()
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        ),
         initialRoute: MyRoutes.homeRoute,
         routes: {
           "/" : ((context) => LoginPage()), //either this or add home: HomePage(), b/w 13 & 14 
