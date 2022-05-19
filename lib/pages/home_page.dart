98f3f9e408d2551ec8d7d2b9e101a287a08aa183
Nitin Jiwnani <jiwnani01@gmail.com>
2022-05-19 17:45:39 +0530

    Cart button number added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_catalog/core/store.dart';
+import 'package:flutter_catalog/models/cart.dart';
 import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/utils/routes.dart';
@@ -46,13 +48,20 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
         backgroundColor: context.canvasColor,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-          backgroundColor: context.theme.buttonColor,
-          child: Icon(CupertinoIcons.cart,
-          color: Colors.white,),
+        floatingActionButton: VxBuilder(
+          mutations: {AddMutation ,RemoveMutation},
+          builder:(context, store, status) => FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+            backgroundColor: context.theme.buttonColor,
+            child: Icon(CupertinoIcons.cart,
+            color: Colors.white,),
+          ).badge(color: Vx.red500, size: 20, count: _cart.items.length, textStyle: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold
+          )),
         ),
         body: SafeArea(
           child: Container(
